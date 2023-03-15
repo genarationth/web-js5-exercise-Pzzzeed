533cd52e13cc54a8923bdddb99fac5a81ebab193
Peeranat Phengsalaeh <seed.peeranat@gmail.com>
2023-03-15 20:53:31 +0700

    array and loop exercise

---
+++ ex1.js
@@ -0,0 +1,13 @@
+//part1
+
+let bankCustomer = ['Sofie', 'David', 'Juan']
+
+bankCustomer.push('Sara', 'Augustin')
+
+console.log(bankCustomer);
+
+bankCustomer.shift()
+
+console.log(bankCustomer);
+
+bankCustomer.splice(1, 0 , 'Renata')
